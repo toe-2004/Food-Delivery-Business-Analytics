02293ee1c67cf0b6893ec69f1bb82177c3c6af9d
Toe Toe Naing <toetoenaing218@gmail.com>
2026-05-25 20:22:06 +0630

    etl quert

---
+++ ETL/etl_process.sql
@@ -0,0 +1,33 @@
+INSERT INTO dim_customer (customer_id, name, city)
+SELECT user_id, name, city
+FROM food_oltp1.users;
+
+INSERT INTO dim_product (product_id, name, category, price)
+SELECT product_id, name, category, price
+FROM food_oltp1.products;
+
+INSERT INTO dim_restaurant (restaurant_id, name, city)
+SELECT restaurant_id, name, city
+FROM food_oltp1.restaurants;
+
+INSERT INTO dim_time (time_id, order_date, day, month, year)
+SELECT 
+    order_id AS time_id,
+    order_date,
+    DAY(order_date),
+    MONTH(order_date),
+    YEAR(order_date)
+FROM food_oltp1.orders;
+
+
+INSERT INTO fact_sales (customer_id, product_id, restaurant_id, time_id, quantity, total_amount)
+SELECT 
+    o.user_id AS customer_id,
+    oi.product_id,
+    o.restaurant_id,
+    o.order_id AS time_id,
+    oi.quantity,
+    (oi.quantity * oi.price) AS total_amount
+FROM food_oltp1.order_items oi
+JOIN food_oltp1.orders o 
+    ON oi.order_id = o.order_id;
